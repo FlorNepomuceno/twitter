32804e993415add89c9ac4ad9bc26ee5025c7640
FlorNepomuceno <flor.esthela93@gmail.com>
2022-04-30 21:14:46 -0500

    Paso 7: Agregando requerimiento 2 (Modificacion y respectiva prueba)

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,9 +1,17 @@
 const User = require('./../../app/models/user')
 
 describe("Tests para User", () =>{
-    test("Creación de User", () =>{
-        const user = new User(1, "FlorNepomuceno", "Flor", "Bio", "dateCreated", "lastUpdated")
+    test("Requerimiento 1: Creación de User", () =>{
+        const user = new User(1, "FlorNepomuceno", "Flor", "Bio")
         expect(user.id).toBe(1)
         expect(user.username).toBe("FlorNepomuceno")
+        expect(user.name).toBe("Flor")
+        expect(user.bio).toBe("Bio")
+    })
+
+    test("Requerimento 2: Fechas en atributos de User",() => {
+        const user = new User(1, "FlorNepomuceno", "Flor", "Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
