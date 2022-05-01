f60cd5ef7297125240f41da96d61c587b41af049
FlorNepomuceno <flor.esthela93@gmail.com>
2022-04-30 20:54:24 -0500

    Paso 6 Agregando clase User y su respectiva prueba

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,5 +1,9 @@
-describe("Prueba d jest", () =>{
-    test("Prueba 1", () =>{
-        expect(10).toBe(9)
+const User = require('./../../app/models/user')
+
+describe("Tests para User", () =>{
+    test("Creación de User", () =>{
+        const user = new User(1, "FlorNepomuceno", "Flor", "Bio", "dateCreated", "lastUpdated")
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("FlorNepomuceno")
     })
 })
