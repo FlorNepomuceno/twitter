b4bed91bfaeddf481d19adcbacd707d52d5e68f8
FlorNepomuceno <flor.esthela93@gmail.com>
2022-05-04 23:37:02 -0500

    2. Prueba de unidad

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,3 +1,4 @@
+const User = require('../../app/models/user')
 const UserService = require('./../../app/services/UserService')
 
 describe("Test de UserService", () => {
@@ -7,5 +8,13 @@ describe("Test de UserService", () => {
         expect(user.name).toBe("Flor")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
+    })
+    test("2. Obtener todos los datos del usuario en una lista", () => {
+        const user = UserService.create(1, "FlorNepomuceno", "Flor")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("FlorNepomuceno")
+        expect(userInfoInList[2]).toBe("Flor")
+        expect(userInfoInList[3]).toBe("Sin bio")
     })
 })
